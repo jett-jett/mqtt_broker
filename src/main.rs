c87cfbd9567899422c7e0fb189fd8c8126f7a8e8
jett-jett <jett.ptacek@gmail.com>
2024-04-02 16:53:34 -0500

    Testing other bind and accept implementation

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
+use std::io::BufRead;
 use std::io::Read;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::io::BufReader;
 
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
-    let listener = TcpListener::bind("127.0.0.1:1883").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-        println!("Connection Established");
+    let listener = TcpListener::bind("127.0.0.1:1883")?;
+    while let Ok((stream, _)) = listener.accept() {
+        println!("New client connected: {:?}", stream.peer_addr());
     }
+    Ok(())
+}
 
+// fn handle_connection(mut stream: TcpStream){
+
+//     let mut buf_reader = BufReader::new(&mut stream);
+//     let req_lines:Vec<_> = buf_reader.by_ref().lines()
+//         .map(|result| result.unwrap_or_else(|_| return "".to_string()))
+//         .take_while(|line| !line.is_empty() )
+//         .collect();
     
-}
+   
+//     for a in req_lines {
+//         println!("Stream: {a}");
+//     }
+    
+//     //let mut buffer = [0;1024];
+//     //stream.read(&mut buffer).unwrap();
 
-fn handle_connection(mut stream: TcpStream){
-    let mut buffer = [0;1024];
-    stream.read(&mut buffer).unwrap();
-
-}
+// }
