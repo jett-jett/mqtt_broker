bf1377832a2611c3d775fcd3f86c25beb46de1a1
jett-jett <jett.ptacek@gmail.com>
2024-04-02 16:19:52 -0500

    Update local address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::Read;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:1883").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+        println!("Connection Established");
+    }
+
+    
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).unwrap();
+
 }
