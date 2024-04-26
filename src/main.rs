750cf3977e0b63a0a7e8a1f9615a1c34ebf21612
jett-jett <jett.ptacek@gmail.com>
2024-04-26 15:50:12 -0500

    Testing tcp listner

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,23 @@ use std::io::BufReader;
 fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:1883")?;
-    while let Ok((stream, _)) = listener.accept() {
+    while let Ok((mut stream, _)) = listener.accept() {
         println!("New client connected: {:?}", stream.peer_addr());
+        let mut buffer = String::new();
+        stream.read_to_string(&mut buffer).expect("Unable to read from buffer");
+        println!("Recieved: {:#04X?}", buffer);
    }
     Ok(())
 }
 
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request: Vec<_> = buf_reader
+        .lines()
+        .collect();
+    println!("Request: {:#?}", request);
+}
 //TODO ADD Inital Connection Response
 //TODO ADD SUBSCRIPING
 //TODO ADD TOPIC
